ff616545760b18522e03ead7f6004c56b7a74791
johncollins0 <johnc1999@hotmail.com>
2020-03-02 19:23:45 +1100

    began development on weapon holding, switching and firing

---
+++ Assets/Scripts/PlayerWeaponController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeaponController : MonoBehaviour
+{
+    public GameObject flute;
+    public GameObject guitar;
+    public GameObject tuba;
+    public GameObject symbal;
+
+    public GameObject[] weaponArray;
+    private Transform[] transformArray;
+    private int[] ammoArray;
+
+    public GameObject rotationPoint;
+    public float weaponSwitchTime;
+    public GameObject activatePoint;
+    public GameObject deactivatePoint;
+
+    private Instrument currentWeapon;
+    private Instrument nextWeapon;
+    private bool changingWeapon = false;
+
+    void Start()
+    {
+        currentWeapon = Instrument.FLUTE;
+
+        weaponArray = new GameObject[] { flute, guitar, tuba, symbal };
+        transformArray = new Transform[] { flute.transform, guitar.transform, tuba.transform, symbal.transform };
+        ammoArray = new int[] { 3, 1, 1, 1 };
+        flute.SetActive(true);
+        guitar.SetActive(false);
+        tuba.SetActive(false);
+        symbal.SetActive(false);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (changingWeapon)
+        {
+            weaponArray[(int)currentWeapon].transform.RotateAround(rotationPoint.transform.position, transform.forward, weaponSwitchTime * Time.deltaTime);
+        }
+        //float deactivateTime;
+        if (Input.GetKeyDown(KeyCode.Alpha1) && currentWeapon != Instrument.FLUTE)
+        {
+            flute.SetActive(true);
+            float deactivateTime = Time.time + weaponSwitchTime;
+        }
+    }
+}
