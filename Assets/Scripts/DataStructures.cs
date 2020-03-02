2ffa0ec5f3abfff956968ed441efe5eac4cd408c
TSF <10264833+TSF404@users.noreply.github.com>
2020-03-02 17:34:20 +1100

    + data structures

---
+++ Assets/Scripts/DataStructures.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum Instrument
+{
+    FLUTE,
+    TUBA,
+    GUITAR
+}
+
+
+
+
+public class DataStructures : MonoBehaviour
+{
+}
---
+++ Assets/Scripts/SoundScheduler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public struct SoundTrack
+{
+    public AudioClip track;
+    public float trackLength;
+}
+
+
+
+
+public class SoundScheduler : MonoBehaviour
+{
+
+    public List<SoundTrack> soundTracks;
+
+
+    void Start()
+    {
+        StartCoroutine(PlayTrack(0));
+    }
+
+    private IEnumerator PlayTrack (int _trackID)
+    {
+        _trackID = _trackID % soundTracks.Count;
+
+        Debug.Log("playing track: " + soundTracks[_trackID].track);
+
+        yield return new WaitForSeconds(soundTracks[_trackID].trackLength);
+
+
+        StartCoroutine(PlayTrack(_trackID+1));
+    }
+
+}
