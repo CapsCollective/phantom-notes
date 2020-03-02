8d150a9e6b213334b904f5b9ed9dd4bd8b25c273
Jonathan Moallem <jdmoallem@gmail.com>
2020-03-02 17:04:23 +1100

    Made pickups move towards the player and delete themselves

---
+++ Assets/Scripts/PickupInstrument.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupInstrument : MonoBehaviour
+{
+    public float pickupDistance;
+    public float hooverDistance;
+    public float hooverSpeed;
+    
+    private PlayerController player;
+    private float i = 0;
+    private float rate = 0;
+
+    private void Start()
+    {
+        player = PlayerController.Instance;
+    }
+    
+    void Update()
+    {
+        var playerDistance = Vector3.Distance(player.transform.position, transform.position);
+        if (playerDistance < pickupDistance)
+        {
+            Destroy(gameObject,0);
+            print("picked up item");
+        }
+        else if (playerDistance < hooverDistance)
+        {
+            rate = 1/hooverSpeed;
+            if (i < 1)
+            {
+                i += Time.deltaTime * rate;
+                transform.position = Vector3.Lerp(transform.position, player.transform.position, i);
+            }
+        }
+    }
+}
