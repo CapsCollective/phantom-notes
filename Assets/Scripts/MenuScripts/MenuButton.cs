4381e4e64ec18205ed7093e88c1b075a2b882c8a
TSF <10264833+TSF404@users.noreply.github.com>
2020-03-03 14:59:27 +1100

    + MAIN MENU IN WHATUP

---
+++ Assets/Scripts/MenuScripts/MenuButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour
+{
+
+    public enum ButtonType { PLAY }
+
+    public ButtonType buttonType;
+
+    private void OnMouseDown()
+    {
+        switch (buttonType)
+        {
+            case ButtonType.PLAY:
+                PlayerPrefs.SetInt("LevelToLoad", 1);
+                UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+                break;
+        }
+    }
+}
