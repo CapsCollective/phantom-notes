f1ca36faffeb0fade49cca9ecd8ce3d6fd7c72c0
johncollins0 <johnc1999@hotmail.com>
2020-03-03 16:02:53 +1100

    increasing tuba projectile and cullTime

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -9,13 +9,27 @@ public class Projectile : MonoBehaviour
     private Rigidbody rb;
 
     private int[] weaponDamages = {5, 3, 15};
+    private float cullTime;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.up * bulletSpeed;
+        if (instrument == Instrument.Tuba)
+        {
+            cullTime = Time.time + 3f;
+        }
     }
-    
+
+    private void Update()
+    {
+        if (instrument == Instrument.Tuba)
+        {
+            if (Time.time >= cullTime) { Destroy(gameObject); }
+            rb.transform.localScale += new Vector3(3 * Time.deltaTime, 0f, 3 * Time.deltaTime);
+        }
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("enemy"))
