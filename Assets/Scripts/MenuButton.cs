6e6aa8b6f5701d33af3317579f44609565c0fa38
Jonathan Moallem <jdmoallem@gmail.com>
2020-03-09 14:46:00 +1100

    Made menu button function as expected

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuButton : MonoBehaviour
 {
@@ -8,6 +10,15 @@ public class MenuButton : MonoBehaviour
     public enum ButtonType { PLAY }
 
     public ButtonType buttonType;
+    public AudioClip clickSound;
+    public AudioClip hoverSound;
+    public bool hoverPlayed;
+    private GameObject text;
+
+    private void Start()
+    {
+        text = transform.GetChild(0).gameObject;
+    }
 
     private void OnMouseDown()
     {
@@ -16,7 +27,24 @@ public class MenuButton : MonoBehaviour
             case ButtonType.PLAY:
                 PlayerPrefs.SetInt("LevelToLoad", 1);
                 UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+                SoundGuy.Instance.PlaySound(Vector3.zero, 1, clickSound);
                 break;
         }
+    }
+
+    private void OnMouseOver()
+    {
+        if (!hoverPlayed)
+        {
+            SoundGuy.Instance.PlaySound(Vector3.zero, 1, hoverSound);
+            hoverPlayed = true;
+        }
+        text.GetComponent<TextMesh>().color = Color.gray;
+    }
+
+    private void OnMouseExit()
+    {
+        hoverPlayed = false;
+        text.GetComponent<TextMesh>().color = Color.white;
     }
 }
